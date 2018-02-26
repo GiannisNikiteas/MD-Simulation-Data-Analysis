c55a0b4d576eb4b3237be924e579e14f183ee776
GiannisNikiteas <giannis_nikiteas@hotmail.com>
2018-02-26 15:27:52 +0000

    updating files

--- PathHandling.py
+++ PathHandling.py
@@ -7,21 +7,18 @@ class OSPaths:
     Managing paths in Windows and Linux
     need to import os and platform
     """
-    # Configure paths to where the MD simulation files are located
-
     # __pathWIN = 'C:/Users/user/Desktop/MD simulation/Archives of Data/'
     # __pathLINUX = '/media/gn/ECD68B8AD68B53AC/Users/user/Desktop/MD simulation/Archives of Data/'
-
-    # Configure relative paths depending on the directory names you are using
-    __pathLINUX = '../Archives of Data/'
-    __pathWIN = '../Archives of Data/tests/10^3/'
+    # This is not a Valid Path anymore, repair disc
+    # Always Launch from MD simulation dir
+    # __pathLINUX = '/media/gn/4C1EE3431EE324A4/Code/C++/MD simulation/Archives of Data/'
+    __pathWIN = '../Archives of Data/tests/4^3/'
+    __pathLINUX = __pathWIN
     __iso = '/Isothermal~'
     __non_iso = '/Non-Isothermal'
-    __density = ['Density 0.5', 'Density 0.8', 'Density 1.2', 'Density 3.6']    # Add the densities you have simulated
+    __density = ['Density 0.5', 'Density 0.8', 'Density 1.2', 'Density 3.6']
     __step = 'step '
-    
-    #   Add the number of steps for different simulations that you have generated
-    __steps_num = ['10', '50', '2500', '3000', '5000', '10000', '12500', '15000', '20000', '5000 v2']
+    __steps_num = ['50', '2500', '3000', '5000', '10000', '12500', '15000', '20000', '5000 v2']
 
     def __init__(self):
         self.OS = platform.system()
