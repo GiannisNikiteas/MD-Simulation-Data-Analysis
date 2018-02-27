5fb8d8c347f3b2f4a1202a291b6cdef526f6c645
GiannisNikiteas <giannis_nikiteas@hotmail.com>
2018-02-27 00:27:35 +0000

    working example of rdf

--- run_class.py
+++ run_class.py
@@ -1,4 +1,4 @@
-from PathHandling import *      # Deprecated
+from PathHandling import *
 from plotting_class import *
 import path
 
@@ -8,7 +8,7 @@ import path
 # TODO: Obvious CON when plotting multiple plots on the same figure
 # -> the final image will have the name of the last plot called (not much you can do about it
 os.chdir('../../Archives of Data/')
-obj = FilePlotting(10000, 1000)  # constructor is left empty
+obj = FilePlotting(10000, 1000)  # steps, particles
 
 my_list = [0, 0.25, 0.50, 0.65, 0.7, 0.75, 0.8, 0.85, 0.90, 0.97, 1.00, 1.1, 1.2,
            1.25, 1.50, 1.75, 2.00, 2.25, 2.50, 2.75, 4.00]
@@ -22,7 +22,10 @@ n_list = [6, 8, 10, 12]
 #     # obj.avg_pressure(i)
 #     # obj.RDF2(6, i)
 #     # obj.savefig('rdf_a0_n6-12.pdf')
-obj.radial_dist_func(0.5,1,6,0)
+
+#                     rho  T   n   a
+obj.radial_dist_func(0.5, 0.5, 8, 0.5)
+obj.radial_dist_func(0.8409, 2, 8, 0.42045)
 
 plt.tight_layout()
 plt.show()
