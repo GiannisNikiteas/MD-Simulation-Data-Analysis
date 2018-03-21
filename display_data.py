dcc6e0483f95180bc4a668afe7195363af7f9a3f
GiannisNikiteas <giannis_nikiteas@hotmail.com>
2018-03-21 08:08:42 +0000

    added bool switches to rdf, potential. scaled rdf, potential
    Signed-off-by: GiannisNikiteas <giannis_nikiteas@hotmail.com>

--- display_data.py
+++ display_data.py
@@ -32,7 +32,7 @@ class Isomorph:
 
 if __name__ == '__main__':
     os.chdir('../../Archives of Data/')
-    obj = FilePlotting(10000, 1000)  # steps, particles
+    obj = FilePlotting(50000, 1000)  # steps, particles
 
     my_list = [0, 0.25, 0.50, 0.65, 0.7, 0.75, 0.8, 0.85, 0.90, 0.97, 1.00, 1.1, 1.2,
                1.25, 1.50, 1.75, 2.00, 2.25, 2.50, 2.75, 4.00]
@@ -44,12 +44,15 @@ if __name__ == '__main__':
     isomorph_line = Isomorph(0.5, 0.5, 0.5, t_iso)
     rho_iso, a_iso = isomorph_line.gen_line(8)
 
-    for i in range(len(t_iso)):
-        obj.rdf(rho_iso[i], t_iso[i], 8, a_iso[i])
+    # for i in range(len(t_iso)):
+    #     obj.scaled_potential(rho_iso[i], 8, a_iso[i])
+    #     obj.rdf(rho_iso[i], t_iso[i], 8, a_iso[i])
 
-    # for i in n_list:
+    for i in n_list:
+        obj.potential(i, 0.5)
+    #     obj.scaled_potential(0.5, i, 0.5)
         # obj.avg_pressure(1, 1, i)
-        # obj.radial_dist_func(1.5, 2, i, 1.5)
+        # obj.rdf(0.5, 0.5, i, 1)
         # obj.diffusion_plot(0.5, 1.0, 12, a_list)
         # obj.avg_pressure(i)
         # obj.RDF2(6, i)
