c3b064f52d068711c6fcb5e3cbe2400ae4d16453
GiannisNikiteas <giannis_nikiteas@hotmail.com>
2018-03-22 20:41:02 +0000

    visual improvements
    Signed-off-by: GiannisNikiteas <giannis_nikiteas@hotmail.com>

--- contour_isomorph.py
+++ contour_isomorph.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 # Potential strength list
-n_list = range(6, 27)
+n_list = list(range(6, 17))
 # List of isomorphic temperatures
 t_iso_line = np.linspace(0.5, 5, 100)
 # Lists for isomorphic density and a
@@ -14,7 +14,7 @@ rho_iso = np.empty((0, len(t_iso_line)))
 a_iso = np.empty((0, len(t_iso_line)))
 t_iso = np.empty((0, len(t_iso_line)))
 # Reference seed with rho=0.5, T=0.5, a=0.5
-isomorph_line = Isomorph(0.5, 0.5, 0.5, t_iso_line)
+isomorph_line = Isomorph(0.5, 0.5, 1.5, t_iso_line)
 
 # Generate matrix of isomorph quantities for various n s
 for i in range(len(n_list)):
@@ -27,9 +27,9 @@ for i in range(len(n_list)):
 fig = plt.figure('3D contour plot')
 ax = fig.gca(projection='3d')
 ax.plot_surface(rho_iso, a_iso, t_iso, rstride=8, cstride=8, alpha=0.3)
-cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='z', cmap=cm.coolwarm)
-cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='x', cmap=cm.coolwarm)
-cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='y', cmap=cm.coolwarm)
+cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='z', offset=np.amin(t_iso), cmap=cm.coolwarm)
+cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='x', offset=np.amin(rho_iso), cmap=cm.coolwarm)
+cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='y', offset=np.amax(a_iso), cmap=cm.coolwarm)
 ax.set_xlabel(r'$\rho$')
 ax.set_xlim(np.amin(rho_iso), np.amax(rho_iso))
 ax.set_ylabel(r'a')
