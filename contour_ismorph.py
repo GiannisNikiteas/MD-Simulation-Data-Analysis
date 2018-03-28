654276134ebd4233a458943ae3f4049d19df2240
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-03-28 16:33:01 +0100

    visual changes
    Signed-off-by: GiannisNikiteas <giannis_nikiteas@hotmail.com>

--- contour_ismorph.py
+++ contour_ismorph.py
@@ -12,11 +12,11 @@ import numpy as np
  As n increases e.g. 25-30, the isomorphs, become more linear, with a more prompt increase of rho, a vs T
 """
 n_list = np.arange(6, 8, dtype=int)
-rho_ref = np.linspace(0.1, 5, 20)
-
+rho_ref = np.linspace(0.1, 5, 20)   # horizontal par
+n = 8
 # List of isomorphic temperatures:
 # num, adjusts horizontal refinement of surface
-t_iso_line = np.linspace(0.5, 5, 10)
+t_iso_line = np.linspace(0.1, 5, 20)
 # Lists for isomorphic density, parameter a and temperature:
 rho_iso = np.empty((0, len(t_iso_line)))
 a_iso = np.empty((0, len(t_iso_line)))
@@ -27,8 +27,8 @@ t_iso = np.empty((0, len(t_iso_line)))
 
 # Generate matrix of isomorph quantities for various n s
 for i in range(len(rho_ref)):
-    isomorph_line = Isomorph(1, rho_ref[i], 0.5, t_iso_line)
-    temp_rho, temp_a = isomorph_line.gen_line(8)
+    isomorph_line = Isomorph(1, rho_ref[i], 2.5, t_iso_line)
+    temp_rho, temp_a = isomorph_line.gen_line(n)
     rho_iso = np.append(rho_iso, [temp_rho], axis=0)
     a_iso = np.append(a_iso, [temp_a], axis=0)
     t_iso = np.append(t_iso, [t_iso_line], axis=0)
@@ -36,9 +36,10 @@ for i in range(len(rho_ref)):
 # Plot contours
 fig = plt.figure('3D contour plot')
 ax = fig.gca(projection='3d')
-# ax.plot_surface(rho_iso, t_iso, a_iso, alpha=0.9)#, cmap=cm.jet)
 ax.plot_wireframe(rho_iso, t_iso, a_iso, alpha=0.9, cmap=cm.jet)
+ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
 
+# Contour projections on 3D plot, enable if needed for visualisation
 # cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='z', offset=np.amin(t_iso), cmap=cm.coolwarm)  # m.coolwarm
 # cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='x', offset=np.amin(rho_iso), cmap=cm.coolwarm, alpha=0.5)
 # cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='y', offset=np.amax(a_iso), cmap=cm.coolwarm, alpha=0.5)
@@ -51,19 +52,25 @@ ax.set_ylim(np.amin(t_iso), np.amax(t_iso))
 ax.set_zlabel(r'a')
 ax.set_zlim(np.amin(a_iso), np.amax(a_iso))
 
-plt.figure('Separate contours')
-plt.subplot(131)    # X. Y, Z
-cset = plt.contourf(rho_iso, t_iso, a_iso)
-plt.xlabel(r'$\rho$')
-plt.ylabel(r'T')
-plt.subplot(132)    # Y, Z, X
-cset = plt.contourf(t_iso, a_iso, rho_iso)
-plt.xlabel(r'T')
-plt.ylabel(r'a')
-plt.subplot(133)    # X, Z, Y
-cset = plt.contourf(rho_iso, a_iso, t_iso)
-plt.xlabel(r'$\rho$')
-plt.ylabel(r'a')
+# Grid background color set to white
+ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+
+# Contour projections plots
+# plt.figure('Separate contours')
+# plt.subplot(131)    # X. Y, Z
+# cset = plt.contourf(rho_iso, t_iso, a_iso)
+# plt.xlabel(r'$\rho$')
+# plt.ylabel(r'T')
+# plt.subplot(132)    # Y, Z, X
+# cset = plt.contourf(t_iso, a_iso, rho_iso)
+# plt.xlabel(r'T')
+# plt.ylabel(r'a')
+# plt.subplot(133)    # X, Z, Y
+# cset = plt.contourf(rho_iso, a_iso, t_iso)
+# plt.xlabel(r'$\rho$')
+# plt.ylabel(r'a')
 
 plt.tight_layout(pad=0.1, h_pad=0, w_pad=0)
 plt.show()
