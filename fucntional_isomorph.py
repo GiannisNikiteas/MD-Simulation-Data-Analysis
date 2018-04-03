ebd086aa2fa855c439ec20aa82b609f01a3b9915
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-04-03 16:31:39 +0100

    contour scattering markers for n, increased plotting capabilities of functional_isomorph.py
    Signed-off-by: GiannisNikiteas <giannis_nikiteas@hotmail.com>

--- fucntional_isomorph.py
+++ fucntional_isomorph.py
@@ -9,30 +9,53 @@ import numpy as np
 """
 
 
-def iso_function(rho, t, rho0=1.5, t0=1.0, a0=0.5, n=8):
+def iso_function(rho, t, rho0=1.0, t0=1.0, a0=0.5, n=8):
     a = a0 * (rho/rho0 * (t0/t) ** (-6/n)) ** (1.0/3.0)
     return a
 
 
+# Arrays of X, Y planes
+rho = np.arange(0.1, 5.0, 0.05)
+t = np.arange(0.1, 5.0, 0.05)
+RHO, T = np.meshgrid(rho, t)
+
 fig = plt.figure('3D contour plot, with function')
 ax = fig.gca(projection='3d')
-
-rho = np.arange(0.1, 5.0, 0.05)
-t = np.arange(0.1, 5.0, 0.05)
-RHO, T = np.meshgrid(rho, t)
-a = np.array([iso_function(rho, t) for rho, t in zip(np.ravel(RHO), np.ravel(T))])
-A = a.reshape(RHO.shape)
-ax.plot_wireframe(RHO, T, A, color='blue', alpha=0.5, rstride=2, cstride=2)
-
+# Simple contour plot, USE FUNCTION plot_isomorph_contour instead
+# a = np.array([iso_function(rho, t) for rho, t in zip(np.ravel(RHO), np.ravel(T))])
+# A = a.reshape(RHO.shape)
+# ax.plot_wireframe(RHO, T, A, color='blue', alpha=0.5, rstride=2, cstride=2)
 ################################################################
-rho = np.arange(0.1, 5.0, 0.05)
-t = np.arange(0.1, 5.0, 0.05)
-RHO, T = np.meshgrid(rho, t)
-a = np.array([iso_function(rho, t, rho0=1, t0=1, a0=1) for rho, t in zip(np.ravel(RHO), np.ravel(T))])
-A = a.reshape(RHO.shape)
-ax.plot_wireframe(RHO, T, A, color='red', alpha=0.8, rstride=2, cstride=2)
 
-################################################################
+
+def label_name(_a0, _n, _rho=None, _t0=None):
+    rho_str = '{:.4f}'.format(_rho)
+    t_str = '{:.4f}'.format(_t0)
+    a_str = '{:.5f}'.format(_a0)
+    n_str = str(_n)
+
+    name_id = r'$\rho$: ' + rho_str + 'T: ' + t_str + 'a: ' + a_str + 'n: ' + n_str
+    if (_rho is None) or (_t0 is None):  # or (_rho and _t0 is None)
+        # No need for case handling when _t0=None and _rho!=None
+        name_id = 'a: ' + a_str + 'n: ' + n_str
+
+    return name_id
+
+
+def plot_isomorph_contour(_rho0, _t0, _a0, _n):
+    """
+    Uses rho and t meshes from outer scope along with RHO, T 2D arrays to plot a wireframe contour
+    :param _rho0: reference density
+    :param _t0: reference temperature
+    :param _a0: reference parameter A
+    :param _n: reference potential strength parameter
+    """
+    a = np.array([iso_function(rho, t, rho0=_rho0, t0=_t0, a0=_a0, n=_n)
+                  for rho, t in zip(np.ravel(RHO), np.ravel(T))])
+    A = a.reshape(RHO.shape)
+    ax.plot_wireframe(RHO, T, A, color='red', alpha=0.8, rstride=2, cstride=2)
+
+
 # Labels and Axis limits
 ax.set_xlabel(r'$\rho$')
 # ax.set_xlim(np.amin(RHO), np.amax(RHO))
