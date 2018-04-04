f9b1d222b7d7feefd2d772699aa83eef25c1b664
GiannisNikiteas <giannis_nikiteas@hotmail.com>
2018-04-04 01:35:34 +0100

    buf fixed concerning the iso_function resulting into wrong contours
    Signed-off-by: GiannisNikiteas <giannis_nikiteas@hotmail.com>

--- contour_rho_ismorph.py
+++ contour_rho_ismorph.py
@@ -16,25 +16,31 @@ rho_ref = np.linspace(0.1, 5, 20)   # horizontal par
 n = 8
 # List of isomorphic temperatures:
 # num, adjusts horizontal refinement of surface
-t_iso_line = np.linspace(0.1, 5, 20)
+t_iso_line = np.linspace(0.01, 5, 5000)
 # Lists for isomorphic density, parameter a and temperature:
 rho_iso = np.empty((0, len(t_iso_line)))
 a_iso = np.empty((0, len(t_iso_line)))
 t_iso = np.empty((0, len(t_iso_line)))
 
+# Plot contours
+fig = plt.figure('3D contour plot')
+ax = fig.gca(projection='3d')
+
 # Generate matrix of isomorph quantities for various rhos
+# todo: This could potentially be wrong
+# todo: The wireframe here is lines through contour planes at some points (don't know why these points)
+# todo: or could be simply wrong. Think through the how does T get fed into the isomorph
+# todo: and what t1 and t2 are used to generate the data
+# todo: examine how I end up with higher densities than max of rho_ref
 for i in range(len(rho_ref)):
-    isomorph_line = Isomorph(1, rho_ref[i], 2.5, t_iso_line)
+    isomorph_line = Isomorph(1, rho_ref[i], 1, t_iso_line)  # This is the reference point, T=1,rho?,a=1
     temp_rho, temp_a = isomorph_line.gen_line(n)
     rho_iso = np.append(rho_iso, [temp_rho], axis=0)
     a_iso = np.append(a_iso, [temp_a], axis=0)
     t_iso = np.append(t_iso, [t_iso_line], axis=0)
+    ax.scatter(rho_ref[i], 1, 1, marker='+', color='red')
 
-# Plot contours
-fig = plt.figure('3D contour plot')
-ax = fig.gca(projection='3d')
-ax.plot_wireframe(rho_iso, t_iso, a_iso, alpha=0.9, color='red')
-
+ax.plot_wireframe(rho_iso, t_iso, a_iso, alpha=0.7, color='cyan')
 # Contour projections on 3D plot, enable if needed for visualisation
 # cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='z', offset=np.amin(t_iso), cmap=cm.coolwarm)  # m.coolwarm
 # cset = ax.contourf(rho_iso, a_iso, t_iso, zdir='x', offset=np.amin(rho_iso), cmap=cm.coolwarm, alpha=0.5)
@@ -42,9 +48,9 @@ ax.plot_wireframe(rho_iso, t_iso, a_iso, alpha=0.9, color='red')
 
 # Labels and Limits
 ax.set_xlabel(r'$\rho$')
-ax.set_xlim(np.amin(rho_iso), np.amax(rho_iso))
+ax.set_xlim(0, np.amax(rho_iso))
 ax.set_ylabel(r'T')
-ax.set_ylim(np.amin(t_iso), np.amax(t_iso))
+ax.set_ylim(0, np.amax(t_iso))
 ax.set_zlabel(r'a')
 ax.set_zlim(np.amin(a_iso), np.amax(a_iso))
 
--- fucntional_isomorph.py
+++ fucntional_isomorph.py
@@ -11,7 +11,7 @@ The file generates a the contour for all the possible parameter a values, for a 
 
 
 def iso_function(rho, t, rho0=1.0, t0=1.0, a0=0.5, n=8):
-    a = a0 * (rho/rho0 * (t0/t) ** (-6/n)) ** (1.0/3.0)
+    a = a0 * (rho/rho0 * ((t0/t) ** (6/n))) ** (1.0/3.0)
     return a
 
 
@@ -64,6 +64,7 @@ def plot_isomorph_contour(_rho0, _t0, _a0, _n, show_projections=False, transpare
     :param _a0: reference parameter A
     :param _n: reference potential strength parameter
     :param show_projections: Shows X, Y, Z projections of the contours
+    :param transparency: alpha parameter for the plots to adjust transparency
     """
     a = np.array([iso_function(rho, t, rho0=_rho0, t0=_t0, a0=_a0, n=_n)
                   for rho, t in zip(np.ravel(RHO), np.ravel(T))])
@@ -92,6 +93,28 @@ def plot_isomorph_contour(_rho0, _t0, _a0, _n, show_projections=False, transpare
 # m = cm.ScalarMappable(cmap=cm.jet, norm=canvas.norm)
 # m.set_array(n_list)
 # fig.colorbar(m, cmap=cm.jet)
+
+#############################################################################
+# PLOTTING RHO vs T vs A vs N with VARYING RHO
+#############################################################################
+# todo: check with other file contour_rho_isomorph.py if correct
+# todo: if any correct or if none correct
+rho = t = None
+rho = t = np.linspace(0.01, 5, 50)
+RHO, T = np.meshgrid(rho, t)
+rho_ref = np.linspace(0.1, 5, 20)   # list of different reference densities
+n = 8
+g = None
+color_map_rho = cm.jet(np.linspace(0, 1, len(rho_ref)))
+for i in range(len(rho_ref)):
+    r_temp = rho_ref[i]
+    g = plot_isomorph_contour(_rho0=r_temp, _t0=1.0, _a0=2.5, _n=8, transparency=0.5)
+    g.set_color(color_map_rho[i])
+
+m = cm.ScalarMappable(cmap=cm.jet, norm=g.norm)
+m.set_array(rho)
+fig.colorbar(m, cmap=cm.jet)
+ax.legend(loc='best', fancybox=True, fontsize='small')
 
 #############################################################################
 # PLOTTING RHO vs T vs A vs N with DIFFERENT A
