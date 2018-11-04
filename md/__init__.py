99be367c1f9e6069d5f838ac3683b1384add9e3b
giannisnikiteas <giannis_nikiteas@hotmail.com>
2018-11-04 19:11:32 +0000

    Separated the RDF analysis to a different class, making the files easier to read

--- md/__init__.py
+++ md/__init__.py
@@ -2,3 +2,4 @@ from md.stat_quantities import StatQ, FileNaming
 from md.state_properties import StateProperties
 from md.visualise_fluid import ParticleVisualisation
 from md.isomorphs import Isomorph
+from md.rdf_analysis_tools import RDFAnalysis
