9b828e18c1cabe7ec728f28bbc7f9961ffd34a3e
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-11-04 18:03:28 +0000

    included the Filenaming class into the module

--- md/__init__.py
+++ md/__init__.py
@@ -1,4 +1,4 @@
-from md.stat_quantities import StatQ
+from md.stat_quantities import StatQ, FileNaming
 from md.state_properties import StateProperties
 from md.visualise_fluid import ParticleVisualisation
 from md.isomorphs import Isomorph
