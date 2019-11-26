71af2174f7737dcd8295146b891a87e97520fb46
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2019-11-26 01:40:51 +0000

    Added some examples for viewing data

--- show_data.py
+++ show_data.py
@@ -1,11 +1,11 @@
 # %%
+from mdtools import ParticleVisualisation
 import os
-import numpy as np
 import matplotlib.pyplot as plt
-import sys
-from mdtools import StatQ, Isomorph, RDFAnalysis
+from mdtools import StatQ, Isomorph, RDFAnalysis, ParticleVisualisation
 
 plt.style.use('default')
+# NOTE: change to your directory where the data is
 os.chdir("/home/gn/Code/MD-simulation/examples/examplebin")
 
 # Lennard Jones pair potential
@@ -32,7 +32,27 @@ run.rdf_plot("bip_simple_run_", 0.5, 0.5, 8, 0.5)
 run.msd("bip_simple_run_", 0.5, 0.5, 8, 0.5)
 run.vaf("bip_simple_run_", 0.5, 0.5, 8, 0.5)
 
+# plt.show()
+# %%
+
+plt.style.use('default')
+os.chdir("/home/gn/Code/MD-simulation/examples/examplebin")
+
+# Plot the fluid visualisation methods
+
+# Lennard Jones pair potential
+run = ParticleVisualisation(5000, 10**3)
+run.particle_plot("lj_simple_run_", 0.5, 0.5)
+run.vector_field("lj_simple_run_", 0.5, 0.5)
+run.vector_field_3d("lj_simple_run_", 0.5, 0.5)
 plt.show()
 
+# %%
 
-#%%
+plt.style.use('default')
+os.chdir("/home/gn/Code/MD-simulation/examples/examplebin")
+
+# Plot the 3D data
+run = ParticleVisualisation(1000, 5**3)
+run.animation3D("3D_view_", 0.5, 0.5, 8, 0.5)
+# %%
